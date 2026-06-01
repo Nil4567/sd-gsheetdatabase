77914d1432c0238c241d7340971884ccc4bf88e5
Nil4567 <venturessuper@gmail.com>
2026-06-01 23:17:21 +0530

    Create valet.js

---
+++ valet.js
@@ -0,0 +1,10 @@
+
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzkZFQxLchA8H95QRAbIlEhKjJ2iD-mgrTAJfayRaSLhI0oQAXWRJ3hJQkVI6LG2qIbdw/exec";
+
+async function login(credentials) {
+  const response = await fetch(SCRIPT_URL, {
+    method: "POST",
+    body: JSON.stringify(credentials)
+  });
+  return response.text();
+}
